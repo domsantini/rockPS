240e73ba9c9e3d175a15a3ef5a3b69ac35d0d363
domsantini <111545540+domsantini@users.noreply.github.com>
2022-12-13 11:55:51 -0800

    Define getComputerSelection function

--- rockPS.js
+++ rockPS.js
@@ -1,23 +1,23 @@
 // Get CPU choice w/ getComputerChoice
     // Set variables for computerSelection, playerSelection and random number
     // Generate random number between 0-2
-    let computerSelection = undefined;
-    let playerSelection = undefined;
-    let num = Math.floor((Math.random() * 3));
+let computerSelection = undefined;
+let playerSelection = undefined;
+let num = Math.floor((Math.random() * 3));
 
     // Given number assign string RPS
+function getComputerChoice(num) {
     if (num === 0) {
         computerSelection = "Rock";
     } else if (num === 1) {
         computerSelection = "Paper";
     } else {
         computerSelection = "Scissors";
-    }
+    }    
+}
 
-    console.log(num);
-    console.log(computerSelection);
-    console.log(playerSelection);
-
+getComputerChoice(num);
+    
 // Get user input w/ playerSelection
 // Play a round of RPS given playerSelection and computerSelection
 // Return winner
