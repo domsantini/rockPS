8166ae4e015b4d06e2e49bf9cbfe1ac9a2160422
domsantini <111545540+domsantini@users.noreply.github.com>
2022-12-13 11:25:38 -0800

    Generate random number to assign computerSelection

---
+++ rockPS.js
@@ -0,0 +1,30 @@
+// Get CPU choice w/ getComputerChoice
+    // Set variables for computerSelection, playerSelection and random number
+    // Generate random number between 0-2
+    let computerSelection = undefined;
+    let playerSelection = undefined;
+    let num = Math.floor((Math.random() * 3));
+
+    // Given number assign string RPS
+    if (num === 0) {
+        computerSelection = "Rock";
+    } else if (num === 1) {
+        computerSelection = "Paper";
+    } else {
+        computerSelection = "Scissors";
+    }
+
+    console.log(num);
+    console.log(computerSelection);
+    console.log(playerSelection);
+
+// Get user input w/ playerSelection
+// Play a round of RPS given playerSelection and computerSelection
+// Return winner
+
+// Check outputs in console
+
+console.log(num);
+console.log(computerSelection);
+console.log(playerSelection);
+
